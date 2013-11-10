983df123c2d70f30d3b5e522e2b85ea25c3c4a00
Marc Ermshaus <marc@ermshaus.org>
2013-11-10 20:44:36 +0100

    Minor. Formatting

--- src/Horne/Module/Debug/Debug.php
+++ src/Horne/Module/Debug/Debug.php
@@ -12,15 +12,15 @@ class Debug extends AbstractModule
         $app = $this->application;
 
         $app->metas->add(new MetaBag(__DIR__ . '/scripts/debug.phtml', $app->config['outputDir'] . '/debug/index.html', [
-            'id' => 'horne-debug-debug',
-            'title' => 'Debug',
-            'type' => 'page',
+            'id'     => 'horne-debug-debug',
+            'title'  => 'Debug',
+            'type'   => 'page',
             'layout' => 'horne-layout-debug',
-            'path' => '/debug/index.html'
+            'path'   => '/debug/index.html'
         ]));
 
         $app->metas->add(new MetaBag(__DIR__ . '/scripts/layout.phtml', $app->config['outputDir'] . '/nothing', [
-            'id' => 'horne-layout-debug',
+            'id'   => 'horne-layout-debug',
             'type' => 'layout'
         ]));
     }
