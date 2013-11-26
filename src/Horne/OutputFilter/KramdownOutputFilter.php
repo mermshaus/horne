e3513af3290bcc822115de4b5035baa6619eecc0
Marc Ermshaus <marc@ermshaus.org>
2013-11-27 00:11:23 +0100

    Make CLI arguments for kramdown filter configurable via _horne.json

--- src/Horne/OutputFilter/KramdownOutputFilter.php
+++ src/Horne/OutputFilter/KramdownOutputFilter.php
@@ -9,11 +9,17 @@ class KramdownOutputFilter extends AbstractOutputFilter
 {
     public function run($content, MetaBag $mb)
     {
+        $args = $this->application->getSetting('filters.kramdown.cmdargs');
+
+        if ($args === null) {
+            $args = '--coderay-css :class --coderay-line-numbers nil';
+        }
+
         $file = __DIR__ . '/bla';
 
         file_put_contents($file, $content);
 
-        $data = shell_exec('kramdown --coderay-css :class --coderay-line-numbers nil < ' . $file);
+        $data = shell_exec('kramdown ' . $args . ' < ' . $file);
 
         unlink($file);
 
